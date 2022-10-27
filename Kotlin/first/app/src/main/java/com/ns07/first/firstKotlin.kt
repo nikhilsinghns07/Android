cf3ddc375d9bb1eae3d15309ef0cdc39b020aaef
Nikhil Kumar Singh <nikhilsinghns01@gmail.com>
2022-10-27 19:48:03 +0530

    Strings and Aray

---
+++ Kotlin/first/app/src/main/java/com/ns07/first/firstKotlin.kt
@@ -0,0 +1,61 @@
+package com.ns07.first
+
+fun main(args:Array<String>)
+{
+// Learing about variables in Kotlin
+
+//    var a = 10
+//    var b = 20
+//    var c = a+b
+//    println(a+b)
+//    a = 16 // var can be re assigned
+//    val d = 19 // val cannot be re assigned in program later.
+
+    // Data Types
+
+//    var a : Boolean = true
+//    var b : Char = 'R'
+//    var c : Byte = 12
+//    var d : Short = -356
+//    var e : Int = 23435
+//    var f : Long = -52325253425L
+//    var g : Float = 5.3463F
+//    var h : Double = 7.3534276464
+//
+//    println(a)
+//    println(b)
+//    println(c)
+//    println(d)
+//    println(e)
+//    println(f)
+//    println(g)
+//    println(h)
+
+//    Type Conversion
+/*
+toByte()
+toShort()
+toInt()
+tolong()
+toFloat()
+toDouble()
+*/
+
+
+//    var x: Byte = 127
+//    var y: Int = x.toInt()
+
+//    String Data Type
+
+//    var a:String = "Hello Kotlin"
+//    var b:String= "Programming"
+//    println(a + b)
+//    var age = 20
+
+//    println("I am " + age + "years old")
+//    println(a.uppercase())
+
+// Arrays
+    var age = arrayOf(1,2,3,4)
+    println("First element of the array: " + age[0])
+}
